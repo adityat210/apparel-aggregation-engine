61261ab116e355537b5a143bfd3bc70c0b4d1e74
Aditya Tiwari <atiwari411@ucsb.edu>
2026-04-05 23:54:40 -0700

    test_refresh.py updates for new data

--- backend/tests/test_refresh.py
+++ backend/tests/test_refresh.py
@@ -1,18 +1,24 @@
 from app.db.session import SessionLocal
-from app.models.product  import Product
+from app.models.product import Product
 from ingestion.refresh import refresh_product_data
 
 def test_refresh_product_data_inserts_expected_products() -> None:
     db = SessionLocal()
 
     try:
-        #deletes any exsisting products
+        # clear
         db.query(Product).delete()
         db.commit()
 
         starting_count = db.query(Product).count()
-        assert starting_count == 0 #confirms table starts at 0
+        assert starting_count == 0
 
+        # refresh
+        refresh_product_data()
+
+        #validate data
+        final_count = db.query(Product).count()
+        assert final_count == 18 
 
     finally:
         db.close()
