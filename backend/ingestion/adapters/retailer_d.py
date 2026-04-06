45aae91074016aacf3059985dd6474b74c372411
Aditya Tiwari <atiwari411@ucsb.edu>
2026-04-05 23:28:09 -0700

    add adapters for sample data

---
+++ backend/ingestion/adapters/retailer_d.py
@@ -0,0 +1,9 @@
+import json
+from pathlib import Path
+
+def load_retailer_c_products() -> list[dict]:
+   
+    file_path = Path(__file__).resolve().parent.parent / 'sample_data' / 'retailer_d_products.json'
+    with open(file_path, 'r') as f:
+        products = json.load(f)
+    return products
---
+++ backend/ingestion/adapters/retailer_f.py
@@ -0,0 +1,8 @@
+import json
+from pathlib import Path
+
+def load_retailer_c_products() -> list[dict]:
+    file_path = Path(__file__).resolve().parent.parent / 'sample_data' / 'retailer_f_products.json'
+    with open(file_path, 'r') as f:
+        products = json.load(f)
+    return products
